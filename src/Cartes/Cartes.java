cc32c3eb598311e61e28da80beda4561380a0780
ivan_uribe <ivan-mauricio.uribe-carrillo@univ-tlse3.fr>
2024-09-25 19:23:27 +0200

    tp1_05_

--- src/Cartes/Cartes.java
+++ src/Cartes/Cartes.java
@@ -1,25 +1,8 @@
 package Cartes;
 
-public abstract class Cartes {
-
-	private static class Configuration extends Cartes {
-
-		private int nbExemplaires;
-		private Cartes carte;
-		
-		public Configuration(int nbExemplaires, Cartes carte) {
-			this.nbExemplaires = nbExemplaires;
-			this.carte = carte;
-		}
-
-		public int getNbExemplaires() {
-			return nbExemplaires;
-		}
-
-		public Cartes getCarte() {
-			return carte;
-		}
-			
+public class Cartes {
+	
+	public Cartes() {	
 	}
 
 }
--- src/Cartes/JeuDeCartes.java
+++ src/Cartes/JeuDeCartes.java
@@ -4,13 +4,44 @@ public class JeuDeCartes {
 	
 	private Configuration[] configuration ;
 	
-	public JeuDeCartes() {
-		this.configuration = new Configuration[19]; // Initialisation du tableau avec 19 éléments
-	}
+    public JeuDeCartes() {
+        this.configuration = new Configuration[]{
+                new Configuration(new Cartes("25KM"), 10),
+                new Configuration(new Cartes("50KM"), 10),
+                new Configuration(new Cartes("75KM"), 10),
+                new Configuration(new Cartes("100KM"), 12),
+                new Configuration(new Cartes("200KM"), 4),
+                new Configuration(new Cartes("Feu Vert"), 14),
+                new Configuration(new Cartes("Fin Limite"), 6),
+                new Configuration(new Cartes("Bidon d'essence"), 6),
+                new Configuration(new Cartes("Roue de secours"), 6),
+                new Configuration(new Cartes("Réparation"), 6),
+                new Configuration(new Cartes("Feu Rouge"), 5),
+                new Configuration(new Cartes("Limite 50"), 4),
+                new Configuration(new Cartes("Panne d'essence"), 3),
+                new Configuration(new Cartes("Crevaison"), 3),
+                new Configuration(new Cartes("Accident"), 3),
+                new Configuration(new Cartes("Prioritaire"), 1),
+                new Configuration(new Cartes("Citerne"), 1),
+                new Configuration(new Cartes("Increvable"), 1),
+                new Configuration(new Cartes("As du volant"), 1)
+        };
+    }
+
+    public String affichageJeuDeCartes() {
+        StringBuilder sb = new StringBuilder("JEU:\n");
+        for (Configuration config : configuration) {
+            sb.append(config.getNbExemplaires())
+                    .append(" ")
+                    .append(config.getCarte())
+                    .append("\n");
+        }
+        return sb.toString();
+    }
 	
 	
 
-	private class Configuration {
+	private static class Configuration {
 
 		private int nbExemplaires;
 		private Cartes carte;
