6f04ef5cc22b3a0a400879213c12f9304fd0bc2a
ivan_uribe <ivan-mauricio.uribe-carrillo@univ-tlse3.fr>
2024-09-25 18:09:57 +0200

    tp1_04_

--- src/Cartes/JeuDeCartes.java
+++ src/Cartes/JeuDeCartes.java
@@ -1,6 +1,33 @@
 package Cartes;
 
 public class JeuDeCartes {
+	
+	private Configuration[] configuration ;
+	
+	public JeuDeCartes() {
+		this.configuration = new Configuration[19]; // Initialisation du tableau avec 19 éléments
+	}
+	
+	
+
+	private class Configuration {
+
+		private int nbExemplaires;
+		private Cartes carte;
+		
+		public Configuration(Cartes carte,int nbExemplaires) {
+			this.carte = carte;
+			this.nbExemplaires = nbExemplaires;
+		}
+
+		public int getNbExemplaires() {
+			return nbExemplaires;
+		}
+		
+		public Cartes getCarte() {
+			return carte;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
