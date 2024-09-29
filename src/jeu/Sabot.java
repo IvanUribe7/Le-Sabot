a063de5440f155433ceef436ce282ec23c47d7e5
ivan_uribe <ivan-mauricio.uribe-carrillo@univ-tlse3.fr>
2024-09-29 17:59:30 +0200

    tp1_09_

--- src/jeu/Sabot.java
+++ src/jeu/Sabot.java
@@ -1,7 +1,8 @@
 package jeu;
 
-import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import Cartes.*;
 
@@ -10,6 +11,7 @@ public class Sabot implements Iterable<Cartes>{
     private Cartes[] cartes;  
     private int nbCartes;   
     private int capaciteMax = 106;
+    private int modCount = 0;
 
     public Sabot(Cartes[] cartes) {
         this.cartes = cartes;
@@ -28,8 +30,51 @@ public class Sabot implements Iterable<Cartes>{
         nbCartes++;
     }
     
+    @Override
     public Iterator<Cartes> iterator() {
         return new SabotIterator();
+    }
+    
+    private class SabotIterator implements Iterator<Cartes> {
+        private int index = 0;
+        private boolean lastRetired = false; 
+        private int expectedModCount = modCount; 
+
+        @Override
+        public boolean hasNext() {
+            return index < nbCartes;
+        }
+
+        @Override 
+        public Cartes next() throws ConcurrentModificationException,NoSuchElementException{
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException("Le sabot a été modifié pendant l'itération.");
+            }
+            if (!hasNext()) {
+                throw new NoSuchElementException("Pas de carte suivante.");
+            }
+            lastRetired = false;
+            return cartes[index++];
+        }
+
+        @Override
+        public void remove() throws ConcurrentModificationException,IllegalStateException{
+        	
+        	if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException("Le sabot a été modifié pendant l'itération.");
+            }
+            if (index == 0 || lastRetired) {
+                throw new IllegalStateException("Impossible de retirer la carte.");
+            }
+            
+            for (int i = index - 1; i < nbCartes - 1; i++) {
+                cartes[i] = cartes[i + 1];
+            }
+            cartes[--nbCartes] = null;
+            lastRetired = true;
+            modCount++;
+            expectedModCount = modCount;  
+        }
     }
 
 }
